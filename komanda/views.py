95f93d8e1131f429e3f2bf34c45d634deb583761
Regis <r.petrauskui@gmail.com>
2023-08-28 13:50:06 +0300

    sukurtas komandu viewsas

--- komanda/views.py
+++ komanda/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Komanda, Zaidejas
+from django.views import generic
+from django.db.models import Q
+# from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    num_komandos = Komanda.objects.all().count()
+    num_zaidejai = Zaidejas.objects.count()
+
+    context_t = {
+        'num_komandos_t': num_komandos,
+        'num_zaidejai_t': num_zaidejai,
+    }
+
+    return render(request, 'index.html', context=context_t)
+
+
+def komandos(request):
+    visos_komandos = Komanda.objects.all()
+    # print(authors)
+    context_t = {
+        'visos_komandos_t': visos_komandos
+    }
+    return render(request, 'visos_komandos.html', context=context_t)
